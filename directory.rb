6e0b873e3c03363ea957769f18622786751ad846
milesillsley <milesillsley@hotmail.co.uk>
2017-03-31 11:48:42 +0100

    8.ex6

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(50)
+  puts "To finish, just hit return twice".center(50)
   # creates an empty array
   students = []
   #get the first name
@@ -8,8 +8,8 @@ def input_students
   # while the name is not empty, repeat his code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: :november, hobbies: :music, COB: :cob, height: :about2m}
+    puts "Now we have #{students.count} students".center(50)
     # get another name from the user
     name = gets.chomp
   end
@@ -19,43 +19,43 @@ end
 
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   count = 1
-  while count != students.length
-      puts "#{count}.#{students[count-1][:name]} (#{students[count-1][:cohort]} cohort)"
+  while count != students.length + 1
+      puts "#{count}. #{students[count-1][:name]} (#{students[count-1][:cohort]} cohort)".center(50)
     count += 1
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(50)
 end
 
 def print_by_letter(students)
-  puts "Please choose letter to print by:"
+  puts "Please choose letter to print by:".center(50)
   letter = gets.chomp
   count = 1
   students.each do |student|
     if student[:name].chr == letter
-      puts "#{count}.#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+      count += 1
     end
-    count += 1
   end
 end
 
 def print_by_name_length(students)
-  puts "Please choose maximum name character length:"
+  puts "Please choose maximum name character length:".center(50)
   length = gets.chomp.to_i
   count = 1
   students.each do |student|
     if student[:name].length <= length
-      puts "#{count}.#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+      count += 1
     end
-    count =+ 1
   end
 end
 
