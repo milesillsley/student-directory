3fbd79e205043f6754fc5b00641cdc07882df6e6
milesillsley <milesillsley@hotmail.co.uk>
2017-04-03 16:11:40 +0100

    14. ex6

--- directory.rb
+++ directory.rb
@@ -92,14 +92,13 @@ def show_students
 end
 
 def save_students(filename = "students.csv")
-    file = File.open(filename, "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name],student[:cohort],student[:hobbies],student[:COB],student[:height]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |f|
+    @students.each do |student|
+      student_data = [student[:name],student[:cohort],student[:hobbies],student[:COB],student[:height]]
+      csv_line = student_data.join(",")
+      f.puts csv_line
+    end
   end
-  file.close
 end
 
 def save_students_as
@@ -140,12 +139,12 @@ def load_students_file
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
-    add_student(name, cohort)
+  File.open(filename, "r") do |f|
+    f.readlines.each do |line|
+      name, cohort = line.chomp.split(",")
+      add_student(name, cohort)
+    end
   end
-  file.close
 end
 
 def add_student(*args)
